204577d643a39a9949828316a71bb4443a525766
EvanPan <EvanPan@boyaa.com>
2019-03-08 16:06:08 +0800

    fix:更新kit版本

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/dy-dayan/common-srv-atomicid/idl/dayan/common/srv-atomicid"
 	"github.com/dy-dayan/common-srv-atomicid/dal/db"
 	"github.com/dy-dayan/common-srv-atomicid/handler"
-	"github.com/dy-gopkg/kit"
 	"github.com/dy-dayan/common-srv-atomicid/util/config"
+	"github.com/dy-gopkg/kit/micro"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	kit.Init()
+	micro.Init()
 
 	// 初始化配置
 	uconfig.Init()
@@ -21,10 +21,10 @@ func main() {
 	//TODO 初始化缓存
 	//cache.CacheInit()
 
-	err := dayan_common_srv_atomicid.RegisterAtomicIDHandler(kit.Server(), &handler.Handler{})
+	err := dayan_common_srv_atomicid.RegisterAtomicIDHandler(micro.Server(), &handler.Handler{})
 	if err != nil {
 		logrus.Fatalf("RegisterPassportHandler error:%v", err)
 	}
 
-	kit.Run()
+	micro.Run()
 }
